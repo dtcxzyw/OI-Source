574537de7ab0b30a404d6ae0de004faf3e624447
dtcxzyw <2601110573@qq.com>
2018-09-27 22:04:45 +0800

    9.27

--- Source/Point-Based Partition/4115.cpp
+++ Source/Point-Based Partition/4115.cpp
@@ -1,146 +1,138 @@
 #include <algorithm>
 #include <cstdio>
-#include <set>
-#include <vector>
 int read() {
     int res = 0, c;
     bool flag = false;
     do {
         c = getchar();
         flag |= c == '-';
-    } while (c < '0' || c > '9');
-    while ('0' <= c && c <= '9') {
+    } while(c < '0' || c > '9');
+    while('0' <= c && c <= '9') {
         res = res * 10 + c - '0';
         c = getchar();
     }
-    return flag ? -res : res;
+    return res;
 }
 int getOp() {
     int c;
     do
         c = getchar();
-    while (c != 'C' && c != 'A');
+    while(c != 'C' && c != 'A');
     return c;
 }
-const int size = 100005, inf = 1 << 28;
+const int size = 100005;
 struct Edge {
-    int to, next, w;
+    int to, nxt, w;
 } E[size * 2];
-int last[size] = {}, cnt = 0;
+int last[size], cnt = 0;
 void addEdge(int u, int v, int w) {
     ++cnt;
-    E[cnt].to = v, E[cnt].next = last[u], E[cnt].w = w;
+    E[cnt].to = v, E[cnt].nxt = last[u], E[cnt].w = w;
     last[u] = cnt;
 }
-int tot, csiz, crt, siz[size];
-bool vis[size] = {};
-int getRoot(int u, int p) {
+bool vis[size];
+int root, tsiz, msiz, siz[size];
+void getRootImpl(int u, int p) {
     int maxs = 0;
     siz[u] = 1;
-    for (int i = last[u]; i; i = E[i].next) {
+    for(int i = last[u]; i; i = E[i].nxt) {
         int v = E[i].to;
-        if (!vis[v] && v != p) {
-            getRoot(v, u);
+        if(!vis[v] && v != p) {
+            getRootImpl(v, u);
             siz[u] += siz[v];
             maxs = std::max(maxs, siz[v]);
         }
     }
-    maxs = std::max(maxs, tot - siz[u]);
-    if (csiz > maxs) {
-        csiz = maxs;
-        crt = u;
+    maxs = std::max(maxs, tsiz - siz[u]);
+    if(maxs < msiz) {
+        msiz = maxs;
+        root = u;
     }
 }
-typedef std::multiset<int> Heap;
-Heap heaps[size];
-int hcnt = 0;
-std::vector<std::pair<int, int> > rts[size];
-void DFS(int heap, int u, int p, int base) {
-    heaps[heap].insert(base);
-    rts[u].push_back(std::make_pair(base, heap));
-    for (int i = last[u]; i; i = E[i].next) {
+int getRoot(int u, int csiz) {
+    msiz = 1 << 30, tsiz = csiz;
+    getRootImpl(u, 0);
+    return root;
+}
+int dis1, c1;
+void DFS(int u, int p, int base) {
+    if(dis1 < base)
+        dis1 = base, c1 = 0;
+    if(dis1 == base)
+        ++c1;
+    for(int i = last[u]; i; i = E[i].nxt) {
         int v = E[i].to;
-        if (!vis[v] && v != p)
-            DFS(heap, v, u, base + E[i].w);
+        if(!vis[v] && v != p)
+            DFS(v, u, base + E[i].w);
     }
 }
-void insert(int u) {
-    for (int i = 0; i < rts[u].size(); ++i) {
-        Heap &heap = heaps[rts[u][i].second];
-        heap.insert(rts[u][i].first);
+struct Info {
+    int maxd, cnt;
+} dp[size];
+void solve(int u) {
+    int dis2, dis3, c2, c3;
+    dis2 = dis3 = c2 = c3 = 0;
+    for(int i = last[u]; i; i = E[i].nxt) {
+        int v = E[i].to;
+        if(!vis[v]) {
+            dis1 = c1 = 0;
+            DFS(v, u, E[i].w);
+            int cdis3 = dis1 + dis2;
+            if(dis3 < cdis3)
+                dis3 = cdis3, c3 = 0;
+            if(dis3 == cdis3)
+                c3 += c1 * c2;
+            if(dis1 > dis2)
+                dis2 = dis1, c2 = 0;
+            if(dis1 == dis2)
+                c2 += c1;
+        }
     }
+    dp[u].maxd = dis3;
+    dp[u].cnt = c3;
 }
-void erase(int u) {
-    for (int i = 0; i < rts[u].size(); ++i) {
-        Heap &heap = heaps[rts[u][i].second];
-        heap.erase(heap.find(rts[u][i].first));
-    }
-}
-std::pair<int, std::vector<int> > src[size];
-int scnt = 0;
+int fa[size];
 void divide(int u) {
+    solve(u);
     vis[u] = true;
-    ++scnt;
-    src[scnt].first = u;
-    std::vector<int> &s = src[scnt].second;
-    for (int i = last[u]; i; i = E[i].next) {
+    for(int i = last[u]; i; i = E[i].nxt) {
         int v = E[i].to;
-        if (!vis[v]) {
-            s.push_back(++hcnt);
-            DFS(s.back(), v, u, E[i].w);
-
-            tot = siz[v], csiz = inf;
-            getRoot(v, 0);
-            divide(crt);
+        if(!vis[v]) {
+            int nrt = getRoot(v, siz[v]);
+            fa[nrt] = u;
+            divide(nrt);
         }
     }
 }
-bool flag[size] = {};
+void update(int u) {}
+bool col[size];
 int main() {
     int n = read();
-    for (int i = 1; i < n; ++i) {
+    for(int i = 1; i < n; ++i) {
         int u = read();
         int v = read();
         int w = read();
         addEdge(u, v, w);
         addEdge(v, u, w);
     }
-    tot = n, csiz = inf;
-    getRoot(1, 0);
-    divide(crt);
-    int q = read(), wcnt = n;
-    for (int i = 0; i < q; ++i) {
-        if (getOp() == 'C') {
+    int rt = getRoot(1, n);
+    divide(rt);
+    int q = read();
+    while(q--) {
+        if(getOp() == 'C') {
             int u = read();
-            flag[u] ^= 1;
-            if (flag[u])
-                erase(u), --wcnt;
-            else
-                insert(u), ++wcnt;
-        } else if (wcnt) {
-            int res = -inf;
-            for (int j = 0; j < scnt; ++j) {
-                int first = -inf, second = -inf;
-                int u = src[j].first;
-                std::vector<int> &s = src[j].second;
-                for (int k = 0; k < s.size(); ++k) {
-                    Heap &heap = heaps[s[k]];
-                    if (heap.size()) {
-                        int val = *heap.rbegin();
-                        if (val > second) {
-                            second = val;
-                            if (second > first) std::swap(first, second);
-                        }
-                    }
-                }
-                if (flag[u])
-                    res = std::max(res, first + second);
-                else
-                    res = std::max(res, std::max(first, 0) + std::max(second, 0));
+            col[u] ^= 1;
+
+            while(u) {
+                update(u);
+                u = fa[u];
             }
-            printf("%d\n", res);
-        } else
-            puts("They have disappeared.");
+        } else {
+            if(dp[rt].cnt)
+                printf("%d\n", dp[rt].maxd);
+            else
+                puts("They have disappeared.");
+        }
     }
     return 0;
 }
