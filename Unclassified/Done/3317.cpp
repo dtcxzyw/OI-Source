de62a6aa641086d857fc335091a267416478fbfc
dtcxzyw <2601110573@qq.com>
2018-10-01 21:51:19 +0800

    10.1

--- Unclassified/Done/3317.cpp
+++ Unclassified/Done/3317.cpp
@@ -1,44 +1,49 @@
-#include <cstdio>
-#include <cmath>
 #include <algorithm>
-const int size=60;
+#include <cmath>
+#include <cstdio>
+const int size = 60;
 double P[size][size];
-const double eps=1e-8;
-double clamp(double x){
-	return x>eps?x:eps;
+const double eps = 1e-8;
+double clamp(double x) {
+    return x > eps ? x : eps;
 }
-double slove(int n){
-	double res=1.0;
-	for(int i=1;i<=n;++i){
-		int base=i;
-		for(int j=i+1;j<=n;++j)if(fabs(P[j][i])>fabs(P[base][i]))base=j;
-		if(i!=base)for(int j=1;j<=n;++j)std::swap(P[i][j],P[base][j]);
-		if(fabs(P[i][i])<eps)return 0.0;
-		res*=P[i][i];
-		for(int j=i+1;j<=n;++j){
-			double t=-P[j][i]/P[i][i];
-			for(int k=i;k<=n;++k)
-				P[j][k]+=P[i][k]*t;
-		}
-	}
-	return fabs(res);
+double slove(int n) {
+    double res = 1.0;
+    for(int i = 1; i <= n; ++i) {
+        int base = i;
+        for(int j = i + 1; j <= n; ++j)
+            if(fabs(P[j][i]) > fabs(P[base][i]))
+                base = j;
+        if(i != base)
+            for(int j = 1; j <= n; ++j)
+                std::swap(P[i][j], P[base][j]);
+        if(fabs(P[i][i]) < eps)
+            return 0.0;
+        res *= P[i][i];
+        for(int j = i + 1; j <= n; ++j) {
+            double t = -P[j][i] / P[i][i];
+            for(int k = i; k <= n; ++k)
+                P[j][k] += P[i][k] * t;
+        }
+    }
+    return fabs(res);
 }
-int main(){
-	int n;
-	scanf("%d",&n);
-	double k=1.0;
-	for(int i=1;i<=n;++i)
-		for(int j=1;j<=n;++j){
-			scanf("%lf",&P[i][j]);
-			double x=clamp(1.0-P[i][j]);
-			if(i<j)k*=x;
-			P[i][j]/=x;
-		}
-	for(int i=1;i<=n;++i)
-		for(int j=1;j<=n;++j)
-			if(i!=j)
-				P[i][i]-=P[i][j];
-	printf("%.8lf\n",slove(n-1)*k);
-	return 0;
+int main() {
+    int n;
+    scanf("%d", &n);
+    double k = 1.0;
+    for(int i = 1; i <= n; ++i)
+        for(int j = 1; j <= n; ++j) {
+            scanf("%lf", &P[i][j]);
+            double x = clamp(1.0 - P[i][j]);
+            if(i < j)
+                k *= x;
+            P[i][j] /= x;
+        }
+    for(int i = 1; i <= n; ++i)
+        for(int j = 1; j <= n; ++j)
+            if(i != j)
+                P[i][i] -= P[i][j];
+    printf("%.8lf\n", slove(n - 1) * k);
+    return 0;
 }
-
--- Unclassified/Done/4768.cpp
+++ Unclassified/Done/4768.cpp
@@ -1,5 +1,4 @@
 #include <algorithm>
-#include <cmath>
 #include <cstdio>
 #include <cstring>
 #include <queue>
@@ -7,8 +6,8 @@ int read() {
     int res = 0, c;
     do
         c = getchar();
-    while (c < '0' || c > '9');
-    while ('0' <= c && c <= '9') {
+    while(c < '0' || c > '9');
+    while('0' <= c && c <= '9') {
         res = res * 10 + c - '0';
         c = getchar();
     }
@@ -34,7 +33,7 @@ struct Node {
     int id;
     Int64 dis;
     Node(int id, Int64 dis) : id(id), dis(dis) {}
-    bool operator<(const Node &rhs) const {
+    bool operator<(const Node& rhs) const {
         return dis > rhs.dis;
     }
 };
@@ -44,15 +43,15 @@ void SSSP(int n) {
     memset(dis, 0x3f, sizeof(Int64) * (n + 1));
     dis[1] = 0;
     heap.push(Node(1, 0));
-    while (heap.size()) {
+    while(heap.size()) {
         int u = heap.top().id;
         Int64 ud = heap.top().dis;
         heap.pop();
-        if (dis[u] == ud) {
-            for (int i = last[u]; i; i = E[i].next) {
+        if(dis[u] == ud) {
+            for(int i = last[u]; i; i = E[i].next) {
                 int v = E[i].to;
                 Int64 vd = ud + E[i].w;
-                if (vd < dis[v]) {
+                if(vd < dis[v]) {
                     dis[v] = vd;
                     heap.push(Node(v, vd));
                 }
@@ -62,12 +61,12 @@ void SSSP(int n) {
 }
 struct Info {
     int u, v, a;
-    bool operator<(const Info &rhs) const {
+    bool operator<(const Info& rhs) const {
         return a > rhs.a;
     }
 };
 class Tree {
-   private:
+private:
     struct Edge {
         int to, next;
     } TE[size * 2];
@@ -82,19 +81,20 @@ class Tree {
     int find(int x) {
         return fa[x] == 0 ? x : fa[x] = find(fa[x]);
     }
-    int p[size * 2][20], a[size * 2], h[size * 2], hend;
+    int p[size * 2][20], a[size * 2], h[size * 2],
+        hend;
     void DFS(int u) {
-        for (int i = 1; i <= hend; ++i)
+        for(int i = 1; i <= hend; ++i)
             p[u][i] = p[p[u][i - 1]][i - 1];
-        for (int i = tlast[u]; i; i = TE[i].next) {
+        for(int i = tlast[u]; i; i = TE[i].next) {
             int v = TE[i].to;
             p[v][0] = u;
             DFS(v);
         }
     }
 
-   public:
-    void init(Info *A, int n, int m) {
+public:
+    void init(Info* A, int n, int m) {
         int siz = sizeof(int) * (2 * n + 1);
         memset(fa, 0, siz);
         memset(tlast, 0, siz);
@@ -103,36 +103,39 @@ class Tree {
         std::sort(A, A + m);
         int icnt = n, end = 2 * n - 1;
         a[0] = -1;
-        for (int i = 0; i < m; ++i) {
+        for(int i = 0; i < m; ++i) {
             int fu = find(A[i].u), fv = find(A[i].v);
-            if (fu != fv) {
+            if(fu != fv) {
                 int id = ++icnt;
                 a[id] = A[i].a;
                 dis[id] = std::min(dis[fu], dis[fv]);
                 addEdge(id, fu);
                 addEdge(id, fv);
                 h[id] = std::max(h[fu], h[fv]) + 1;
-                if (icnt == end) break;
+                if(icnt == end)
+                    break;
             }
         }
         hend = 0;
-        while ((1 << (hend + 1)) < h[icnt] + 1) ++hend;
+        while((1 << (hend + 1)) < h[icnt] + 1)
+            ++hend;
         DFS(icnt);
     }
     Int64 query(int u, int k) {
-        for (int i = hend; i >= 0; --i) {
+        for(int i = hend; i >= 0; --i) {
             int fu = p[u][i];
-            if (a[fu] > k) u = fu;
+            if(a[fu] > k)
+                u = fu;
         }
         return dis[u];
     }
 } T;
 Info A[maxM];
-Int64 foo() {
+void foo() {
     int n = read();
     int m = read();
     initGraph(n);
-    for (int i = 0; i < m; ++i) {
+    for(int i = 0; i < m; ++i) {
         int u = read();
         int v = read();
         int l = read();
@@ -147,16 +150,18 @@ Int64 foo() {
     int k = read();
     int s = read();
     Int64 lastAns = 0;
-    while (q--) {
+    while(q--) {
         int v = (read() + lastAns - 1) % n + 1;
         int p = (read() + lastAns) % (s + 1);
         Int64 res = T.query(v, p);
         printf("%lld\n", res);
-        if (k) lastAns = res;
+        if(k)
+            lastAns = res;
     }
 }
 int main() {
     int t = read();
-    while (t--) foo();
+    while(t--)
+        foo();
     return 0;
 }
