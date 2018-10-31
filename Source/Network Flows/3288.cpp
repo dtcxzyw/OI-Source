2b1d03e4fd2f91f09130836d5652e93806d353d9
dtcxzyw <2601110573@qq.com>
2018-10-31 22:15:27 +0800

    10.31

--- Source/Network Flows/3288.cpp
+++ Source/Network Flows/3288.cpp
@@ -12,110 +12,70 @@ int read() {
     }
     return res;
 }
-const int size = 5050, esiz = size + 3005 * 2;
+const int size = 5005;
+typedef double FT;
 struct Edge {
-    int to, nxt, f, w;
-} E[2 * esiz];
-int last[size], cnt;
-void addEdgeImpl(int u, int v, int f, int w) {
+    int to, nxt, w;
+} E[6005];
+int last[size], cnt = 0;
+void addEdge(int u, int v, int w) {
     ++cnt;
-    E[cnt].to = v, E[cnt].nxt = last[u], E[cnt].f = f,
-    E[cnt].w = w;
+    E[cnt].to = v, E[cnt].nxt = last[u], E[cnt].w = w;
     last[u] = cnt;
 }
-void addEdge(int u, int v, int f, int w) {
-    addEdgeImpl(u, v, f, w);
-    addEdgeImpl(v, u, 0, -w);
-}
-int q[size], dis[size], eid[size];
-bool flag[size];
-bool SPFA(int siz, int S, int T) {
-    memset(dis + 1, 0x3f, sizeof(int) * siz);
-    int b = 0, e = 1;
-    q[b] = S, flag[S] = true, dis[S] = 0;
-    while(b != e) {
-        int u = q[b++];
-        if(b == size)
-            b = 0;
-        flag[u] = false;
-        for(int i = last[u]; i; i = E[i].nxt) {
-            int v = E[i].to;
-            if(E[i].f && dis[v] > dis[u] + E[i].w) {
-                dis[v] = dis[u] + E[i].w;
-                eid[v] = i;
-                if(!flag[v]) {
-                    flag[v] = true;
-                    q[e++] = v;
-                    if(e == size)
-                        e = 0;
-                }
-            }
+FT dis[size], ck;
+bool flag[size], vis[size];
+bool SPFA(int u) {
+    vis[u] = flag[u] = true;
+    for(int i = last[u]; i; i = E[i].nxt) {
+        int v = E[i].to;
+        FT cd = dis[u] + E[i].w + ck;
+        if(cd < dis[v]) {
+            dis[v] = cd;
+            if(flag[v] || SPFA(v))
+                return true;
         }
     }
-    return dis[T] != 0x3f3f3f3f;
+    flag[u] = false;
+    return false;
 }
-int MCMF(int siz, int S, int T) {
-    int res = 0;
-    while(SPFA(siz, S, T)) {
-        int p = T, cf = 1 << 30;
-        while(p != S) {
-            int e = eid[p];
-            cf = std::min(cf, E[e].f);
-            p = E[e ^ 1].to;
+bool check(FT k, int n) {
+    ck = k;
+    memset(vis + 1, 0, sizeof(bool) * n);
+    memset(flag + 1, 0, sizeof(bool) * n);
+    for(int i = 1; i <= n; ++i)
+        if(!vis[i]) {
+            if(SPFA(i))
+                return true;
         }
-        p = T;
-        while(p != S) {
-            int e = eid[p];
-            E[e].f -= cf, E[e ^ 1].f += cf;
-            p = E[e ^ 1].to;
-        }
-        res += dis[T] * cf;
-    }
-    printf("%d\n", res);
-    return res;
-}
-struct Info {
-    int u, v, a, b, c, d;
-} A[3005];
-double check(int k, int n, int m) {
-    memset(last + 1, 0, sizeof(int) * (n + 2));
-    cnt = 1;
-    int S = n + 1, T = n + 2;
-    for(int i = 0; i < m; ++i) {
-        if(A[i].u != S) {
-            addEdge(A[i].u, A[i].v, 1 << 20, A[i].b);
-            addEdge(A[i].v, A[i].u, A[i].c, A[i].a);
-        } else
-            addEdge(S, A[i].v, k, 0);
-    }
-    return static_cast<double>(-MCMF(n + 2, S, T)) / k;
+    return false;
 }
 int main() {
     int n = read();
     int m = read();
+    int S = n + 1;
     for(int i = 0; i < m; ++i) {
-        A[i].u = read();
-        A[i].v = read();
-        A[i].a = read();
-        A[i].b = read();
-        A[i].c = read();
-        A[i].d = read();
-        A[i].a -= A[i].d;
-        A[i].b += A[i].d;
+        int u = read();
+        int v = read();
+        int a = read();
+        int b = read();
+        int c = read();
+        int d = read();
+        if(u != S) {
+            if(c)
+                addEdge(v, u, a - d);
+            addEdge(u, v, b + d);
+        }
     }
-    int l = 1, r = 1 << 20;
-    while(l + 6 < r) {
-        int lm = (l * 2 + r) / 3, rm = (l + r * 2) / 3;
-        double lans = check(lm, n, m),
-               rans = check(rm, n, m);
-        if(lans < rans)
-            l = lm;
+    n += 2;
+    FT l = 0.0, r = 1e5;
+    while(r - l > 1e-4) {
+        FT mid = (l + r) * 0.5;
+        if(check(mid, n))
+            l = mid;
         else
-            r = rm;
+            r = mid;
     }
-    double ans = 0.0;
-    for(int i = l; i <= r; ++i)
-        ans = std::max(ans, check(i, n, m));
-    printf("%.2lf\n", ans);
+    printf("%.2lf\n", l);
     return 0;
 }
